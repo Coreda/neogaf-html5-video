ce42188841c61e9e97e1b9b45a2c3e44562bf136
Coreda <Coreda@users.noreply.github.com>
2016-09-07 05:59:48 +1000

    Update neogaf-html5-video.user.js

--- userscript/neogaf-html5-video.user.js
+++ userscript/neogaf-html5-video.user.js
@@ -4,8 +4,8 @@
 // @version         1.0
 // @description     Enables embedded WebM/Mp4 support on NeoGAF.com
 // @homepageURL     https://github.com/Coreda/neogaf-html5-video/
-// @updateURL       https://github.com/Coreda/neogaf-html5-video/raw/master/userscript/neogaf-html5-meta.js
-// @downloadURL     https://github.com/Coreda/neogaf-html5-video/raw/master/userscript/neogaf-html5-video.js
+// @updateURL       https://github.com/Coreda/neogaf-html5-video/raw/master/userscript/neogaf-html5-video.meta.js
+// @downloadURL     https://github.com/Coreda/neogaf-html5-video/raw/master/userscript/neogaf-html5-video.user.js
 // @icon            https://raw.githubusercontent.com/Coreda/neogaf-html5-video/master/icon48.png
 // @icon64          https://raw.githubusercontent.com/Coreda/neogaf-html5-video/master/icon64.png
 // @include         http://neogaf.com/*
