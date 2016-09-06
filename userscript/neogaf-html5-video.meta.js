a714f42f15993af12482170b37a3bd5418fb8e8f
Coreda <Coreda@users.noreply.github.com>
2016-09-07 06:11:48 +1000

    updated icon links

--- userscript/neogaf-html5-video.meta.js
+++ userscript/neogaf-html5-video.meta.js
@@ -6,8 +6,8 @@
 // @homepageURL     https://github.com/Coreda/neogaf-html5-video/
 // @updateURL       https://github.com/Coreda/neogaf-html5-video/raw/master/userscript/neogaf-html5-video.meta.js
 // @downloadURL     https://github.com/Coreda/neogaf-html5-video/raw/master/userscript/neogaf-html5-video.user.js
-// @icon            https://raw.githubusercontent.com/Coreda/neogaf-html5-video/master/icon48.png
-// @icon64          https://raw.githubusercontent.com/Coreda/neogaf-html5-video/master/icon64.png
+// @icon            https://raw.githubusercontent.com/Coreda/neogaf-html5-video/master/images/icon48.png
+// @icon64          https://raw.githubusercontent.com/Coreda/neogaf-html5-video/master/images/icon64.png
 // @include         http://neogaf.com/*
 // @include         http://www.neogaf.com/*
 // @include         http://neogaf.net/*
