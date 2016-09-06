0b3ffef252be2bf4d3129021730b9914b36dac87
Coreda <Coreda@users.noreply.github.com>
2016-09-07 05:58:34 +1000

    Update and rename neogaf-html5-video.js to neogaf-html5-video.user.js

--- userscript/neogaf-html5-video.user.js
+++ userscript/neogaf-html5-video.user.js
@@ -1,10 +1,17 @@
 // ==UserScript==
 // @name            WebM/Mp4 Embedder for NeoGAF
-// @namespace   
+// @namespace       https://github.com/Coreda/neogaf-html5-video/
+// @version         1.0
 // @description     Enables embedded WebM/Mp4 support on NeoGAF.com
+// @homepageURL     https://github.com/Coreda/neogaf-html5-video/
+// @updateURL       https://github.com/Coreda/neogaf-html5-video/raw/master/userscript/neogaf-html5-meta.js
+// @downloadURL     https://github.com/Coreda/neogaf-html5-video/raw/master/userscript/neogaf-html5-video.js
+// @icon            https://raw.githubusercontent.com/Coreda/neogaf-html5-video/master/icon48.png
+// @icon64          https://raw.githubusercontent.com/Coreda/neogaf-html5-video/master/icon64.png
 // @include         http://neogaf.com/*
 // @include         http://www.neogaf.com/*
-// @version         0.1
+// @include         http://neogaf.net/*
+// @include         http://www.neogaf.net/*
 // @grant           none
 // ==/UserScript==
 
